d8f3bfac8eb2de738b5dbb93c8f8e1446b160987
Nicolas Dobrowolski <nico.dobrowolski@gmail.com>
2022-03-05 15:24:56 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_BASE_URL}/blog`).then((res) => {
+            setBlogs(res.data);
+        });
+    }, []);
+    return (
+        <div className="App">
+            <h4>Article de blog</h4>
+            <dl>
+                {blogs.map((blog) => {
+                    return (
+                        <>
+                            <dd>{blog.titre}</dd>
+                            <dd>{blog.description}</dd>
+                            <hr></hr>
+                        </>
+                    );
+                })}
+            </dl>
+        </div>
+    );
 }
 
 export default App;
